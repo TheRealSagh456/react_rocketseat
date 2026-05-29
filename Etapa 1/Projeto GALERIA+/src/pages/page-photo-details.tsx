a20937c3956193e3b924748ba64629df51f17026
Samel <samuelgoncalvescmps@gmail.com>
2026-05-29 17:48:41 -0300

    +Components +Routes

---
+++ Etapa 1/Projeto GALERIA+/src/pages/page-photo-details.tsx
@@ -0,0 +1,15 @@
+import { useParams } from "react-router";
+import Text from "../components/text";
+
+
+export default function PagePhotoDetails() {
+    const {id} = useParams()
+
+
+    return <>
+        <Text variant="heading-medium">Detalhe da Foto</Text>
+        <hr />
+        <Text variant="heading-medium">ID da Foto: {id}</Text>
+        
+    </>
+}
